555a796c0c8585da73c4690784d316bd5f365855
Jeffrey Young <jmy6342@gmail.com>
2025-10-24 16:41:25 -0400

    start: add flake

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  description = "A devShell example";
+
+  inputs = {
+    nixpkgs.url      = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url  = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+      in
+      {
+        devShells.default = with pkgs; mkShell {
+          packages = [
+            rust-analyzer
+          ];
+
+          buildInputs = [
+            openssl
+            pkg-config
+            rust-bin.nightly.latest.default
+          ];
+
+          shellHook = ''
+            # alias ls=eza
+            # alias find=fd
+          '';
+        };
+      }
+    );
+}
